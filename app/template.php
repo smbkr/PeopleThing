dac1ca3796f68463127557dbadbbd6880ddc1759
Stuart Baker <stuart.baker@trendzer.com>
2016-09-12 11:12:23 +0100

    update code comments

--- app/template.php
+++ app/template.php
@@ -6,6 +6,13 @@ class Template {
 	private function __construct() {}
 	private function __clone() {}
 
+	/**
+	* Returns path to template files
+	*
+	* @param string $template name of the template
+	*
+	* @return string
+	*/
 	public static function is($template) {
 		return ROOTDIR.DS.'app'.DS.'views'.DS.$template.'.phtml';
 	}
